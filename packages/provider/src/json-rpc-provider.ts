b82a1de893f1fbe04f9ea51d38ad05727f354633
rBurgett <ryan@burgettdev.net>
2023-08-28 02:17:47 -0400

    Remove undici

--- packages/provider/src/json-rpc-provider.ts
+++ packages/provider/src/json-rpc-provider.ts
@@ -1,6 +1,6 @@
 import AbortController from 'abort-controller'
 import debug from 'debug'
-import { fetch, Response } from 'undici'
+import { fetch, Response } from 'whatwg-fetch';
 import {
   Account,
   AccountWithTransactions,

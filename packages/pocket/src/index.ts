81d984d5b3bd497413dbce900ca5d510d2cb31a2
rBurgett <ryan@burgettdev.net>
2023-09-02 18:26:36 -0400

    Export packages as monorepo

--- packages/pocket/src/index.ts
+++ packages/pocket/src/index.ts
@@ -1,5 +1,5 @@
-export class Pocket {
-  public static hi() {
-    console.log('gm')
-  }
-}
+export * from "@pokt-foundation/pocketjs-provider";
+export * from "@pokt-foundation/pocketjs-relayer";
+export * from "@pokt-foundation/pocketjs-signer";
+export * from "@pokt-foundation/pocketjs-transaction-builder";
+export * from "@pokt-foundation/pocketjs-utils";
